0e6b60f6123f18aebe3b9cd4fb469811365be43a
wanessabarroslino <wanessabarroslino@gmail.com>
2023-12-20 11:01:52 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
+// CSS
 import './App.css';
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// data
+import { wordsList } from "./data/words"
+
+// components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id:1, name: "start" },
+  { id:2, name: "game" },
+  { id:3, name: "end" }
+];
+
+const guessesQty = 3;
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0)
+
+
+  const pickWordCategory = useCallback(() => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word = 
+      words[category][Math.floor(Math.random() * words[category].length)];  
+
+    return { word, category };
+  }, [words]);
+
+  // starts the secret word game
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLettersState();
+
+    // pick word and pick category
+    const { word, category } = pickWordCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  }, [pickWordCategory]);
+
+  // process the letter input
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    // check if letter has already been utilized
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    // push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+
+  const clearLettersState = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  // check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      // clear all states
+      clearLettersState();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
+
+  // check win condition
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)];
+
+    // win condition
+    if (guessedLetters.length === uniqueLetters.length) {
+      //add score
+      setScore((actualScore) => (actualScore += 100));
+
+      // restart game with new word
+      startGame();
+    }
+
+  }, [guessedLetters, letters, startGame])
+
+  // restarts the game
+  const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
+    setGameStage(stages[0].name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && (
+        <Game 
+          verifyLetter={verifyLetter} 
+          pickedWord={pickedWord} 
+          pickedCategory={pickedCategory} 
+          letters={letters} 
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
---
+++ src/data/words.js
@@ -0,0 +1,15 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Farol", "Volante", "Banco", "Retrovisor", "Freio", "Suspensão", "Câmbio"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Morango", "Uva", "Abacaxi", "Melancia", "Kiwi", "Pêssego", "Ameixa"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Boca", "Nariz", "Orelha", "Dedos", "Cabelo", "Joelho", "Tornozelo"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa-mãe", "HD", "Processador", "Memória RAM", "Placa de vídeo", "Fonte de alimentação", "SSD", "Cooler"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Node.js", "HTML", "CSS", "API", "Biblioteca", "Backend", "Frontend", "Python"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Vegetais", "Frango", "Peixe", "Frutas", "Pão", "Macarrão", "Sushi"],
+  instrumento: ["Violino", "Piano", "Guitarra", "Flauta", "Trompete", "Bateria", "Saxofone", "Violão", "Baixo", "Harmônica", "Acordeão", "Teclado"],
+  esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Vôlei", "Atletismo", "Ciclismo", "Golfe", "Surfe", "Boxe", "Mergulho", "Esqui"],
+  país: ["Brasil", "Estados Unidos", "Japão", "Índia", "França", "Itália", "Canadá", "Austrália", "China", "Rússia", "Alemanha", "México"],
+  profissao: ["Médico", "Professor", "Engenheiro", "Advogado", "Cientista", "Artista", "Programador", "Ator", "Policial", "Jornalista", "Chef", "Piloto"],
+  filme: ["Matrix", "Titanic", "Jurassic Park", "Star Wars", "Inception", "The Godfather", "Avatar", "The Shawshank Redemption", "Pulp Fiction", "The Dark Knight", "The Matrix Reloaded", "Forrest Gump"],
+  animal: ["Leão", "Elefante", "Girafa", "Tigre", "Gato", "Cachorro", "Pássaro", "Tubarão", "Macaco", "Cobra", "Lobo", "Urso"],
+  planta: ["Rosa", "Girassol", "Orquídea", "Folha de Figo", "Samambaia", "Suculenta", "Bambu", "Cacto", "Margarida", "Lírio", "Hortelã", "Girassol"]
+};
